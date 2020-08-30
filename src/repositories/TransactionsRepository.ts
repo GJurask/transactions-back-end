cd3b0b7b2cdbe0c98228074abdb5ef64ef3c5b15
Gabriel Jurask <gjurask@gmail.com>
2020-08-29 23:00:53 -0300

    Made the whole implementation. Tests ok.

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -13,16 +19,65 @@ class TransactionsRepository {
     this.transactions = [];
   }
 
+  // eslint-disable-next-line class-methods-use-this
   public all(): Transaction[] {
-    // TODO
+    if (this.transactions.length === 0) {
+      throw Error('No transactions were made');
+    }
+    return this.transactions;
   }
 
+  // eslint-disable-next-line class-methods-use-this
   public getBalance(): Balance {
-    // TODO
+    /*
+    const balance: Balance = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
+    // eslint-disable-next-line array-callback-return
+    this.transactions.map(transaction => {
+      if (transaction.type === 'income') {
+        balance.income += transaction.value;
+        balance.total += transaction.value;
+      } else {
+        balance.outcome += transaction.value;
+        balance.total -= transaction.value;
+      }
+    });
+      */
+    const balance = this.transactions.reduce(
+      (accumulator: Balance, transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            accumulator.income += transaction.value;
+            break;
+          case 'outcome':
+            accumulator.outcome += transaction.value;
+            break;
+          default:
+            break;
+        }
+
+        accumulator.total = accumulator.income - accumulator.outcome;
+        return accumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  // eslint-disable-next-line class-methods-use-this
+  public create({ title, type, value }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, type, value });
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,24 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, type, value }: Request): Transaction {
+    if (type.toLowerCase() === 'income' || type.toLowerCase() === 'outcome') {
+      if (type.toLocaleLowerCase() === 'outcome') {
+        const balance = this.transactionsRepository.getBalance();
+        const newTotal = balance.total - value;
+        if (newTotal < 0) throw Error('Cannot withdraw more than you have.');
+      }
+
+      const transaction = this.transactionsRepository.create({
+        title,
+        type,
+        value,
+      });
+
+      return transaction;
+    }
+
+    throw Error('Invalid type. Try income or outcome');
   }
 }
 
